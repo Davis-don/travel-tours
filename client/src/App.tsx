bb215fab97db7d3688b1477e90a40dfd2ab3bf6c
Davis-don <davismugoikou@gmail.com>
2025-05-29 08:06:16 +0300

    corrected error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/Homepage';
+// import Homepage from './pages/Homepage';
 import Layout from './components/Layout/Layout';
 import ToursPage from './pages/ToursPage';
 import Aboutpage from './pages/Aboutpage/Aboutpage';
